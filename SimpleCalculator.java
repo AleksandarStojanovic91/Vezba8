ae708ba230fe56fee6bfddd209f5efb019a3f2c4
Aleksandar Stojanovic <31828736+AleksandarStojanovic91@users.noreply.github.com>
2020-07-17 03:07:59 +0200

    Add files via upload

---
+++ SimpleCalculator.java
@@ -0,0 +1,13 @@
+import java.io.Console;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Console console = System.console();
+        System.out.println("Enter first operand:");
+        String op1 = console.readLine();
+        System.out.println("Enter second operand:");
+        String op2 = console.readLine();
+        System.out.println("Result is:");
+        System.out.println(Integer.parseInt(op1)+Integer.parseInt(op2));
+    }
+}
